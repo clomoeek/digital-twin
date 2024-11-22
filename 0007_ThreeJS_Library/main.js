fd4eb5e50d2807a24892983226c7a998c2eb99a9
clomoeek <eekclomo@gmail.com>
2024-11-22 15:33:48 +0900

    20241122

--- 0007_ThreeJS_Library/main.js
+++ 0007_ThreeJS_Library/main.js
@@ -13,43 +13,72 @@ const scene = new THREE.Scene();
 // 배경색 설정
 scene.background = new THREE.Color(0x808080); // 회색 (#808080)
 
+// perspective 카메라 생성
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); // 원근 카메라 생성
-camera.position.set(0, 1, 5); // 카메라 위치 설정
+// 카메라 위치 설정
+camera.position.set(0, 1, 5);
 
-const renderer = new THREE.WebGLRenderer(); // WebGL 렌더러 생성
-renderer.setSize(window.innerWidth, window.innerHeight); // 렌더러 크기를 윈도우 크기에 맞게 설정
-document.body.appendChild(renderer.domElement); // 렌더러의 DOM 요소를 문서에 추가
+// WebGL renderer 생성 : scene과 camera를 받아서 실제 보여지는 이미지를 생성
+const renderer = new THREE.WebGLRenderer();
 
-const controls = new OrbitControls(camera, renderer.domElement); // 카메라를 조작할 수 있는 OrbitControls 생성
+// renderer 크기를 윈도우 크기에 맞게 설정
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-const directLight1 = new THREE.DirectionalLight(0xffffff, 1); // 첫 번째 방향성 조명 생성
-scene.add(directLight1); // 조명을 장면에 추가
-directLight1.position.set(0, 5, 0); // 조명의 위치 설정
+// html 문서의 body요소에 three.js renderer의 DOM 요소를 추가
+// DOM(document object model)은 HTML, XML 문서의 구조화된 표현이며,
+// 웹 브라우저가 문서를 표현하고 접근 및 조작할 수 있도록 제공하는 프로그래밍 인터페이스이다.
+document.body.appendChild(renderer.domElement);
 
-const directLight2 = new THREE.DirectionalLight(0xffffff, 1); // 두 번째 방향성 조명 생성
-scene.add(directLight2); // 조명을 장면에 추가
-directLight2.position.set(5, 0, 0); // 조명의 위치 설정
 
-const directLight3 = new THREE.DirectionalLight(0xffffff, 1); // 세 번째 방향성 조명 생성
-scene.add(directLight3); // 조명을 장면에 추가
-directLight3.position.set(0, 0, 5); // 조명의 위치 설정
+// AxesHelper(length of axis) 생성 : 축을 시각화.
+// X: Red, Y: Green, Z: Blue
+const axesHelper = new THREE.AxesHelper(100);
 
-let model; // 모델을 저장할 변수를 선언
-const raycaster = new THREE.Raycaster(); // 레이캐스터 생성 (객체와의 교차 감지를 위해 사용)
-const mouse = new THREE.Vector2(); // 마우스 위치를 저장할 Vector2 객체 생성
-let previousMaterial = null; // 이전에 선택된 객체의 재질을 저장할 변수
-let mouseInside = false; // 마우스가 화면 안에 있는지 여부를 나타내는 변수
+// AxesHelper를 scene에 추가함
+scene.add(axesHelper);
 
-const loader = new GLTFLoader(); // GLTFLoader 생성
+// 카메라를 조작할 수 있는 OrbitControls 생성
+const controls = new OrbitControls(camera, renderer.domElement); 
+
+
+// AbientLight(color, intensity) 생성 : 전역으로 빛을 발산, 그림자 X
+const ambientLight = new THREE.AmbientLight(0xffffff, 5);
+
+// AbientLight을 scene에 추가하고 위치를 설정
+scene.add(ambientLight);
+ambientLight.position.set(0, 0, 0);
+
+
+// DirectionalLight(color, intensity) 생성 : 방향성 광원, 그림자 O
+const directLight1 = new THREE.DirectionalLight(0xffffff, 3);
+
+// DirectionalLight scene에 추가하고 위치를 설정
+scene.add(directLight1);
+directLight1.position.set(0, 10, 0);
+
+// 모델을 저장할 변수 선언
+let model;
+
+
+// GLTFLoader 생성
+const loader = new GLTFLoader(); 
+
+// glb load 시작
 loader.load(
-    './assets/untitled2.glb', // 로드할 3D 모델 파일 경로
-    (gltf) => {
-        const model = gltf.scene; // 모델을 장면(scene) 객체에서 가져옴
 
-        const textureLoader = new THREE.TextureLoader(); // 텍스처 로더 생성
+    // load 할 3d model file 경로 설정
+    './assets/untitled2.glb', (glb) => {
+
+        const model = glb.scene; // 모델을 장면(scene) 객체에서 가져옴
+
+        // TextureLoader를 생성
+        const textureLoader = new THREE.TextureLoader();
+
+        // texture load 시작
         const metalnessTexture = textureLoader.load(
-            './assets/MetalPlates014_1K-PNG_Metalness.png', // 금속 텍스처 경로
+            // load 할 texture file 경로 설정
+            './assets/MetalPlates014_1K-PNG_Metalness.png',
             () => console.log("success"), // 성공 시 콘솔에 메시지 출력
             undefined,
             (error) => console.log("error", error) // 에러 발생 시 콘솔에 메시지 출력
@@ -63,7 +92,8 @@ loader.load(
             }
         });
 
-        scene.add(model); // 모델을 장면에 추가
+        // 모델을 scene에 추가
+        scene.add(model);
     },
     undefined,
     (error) => {
@@ -71,6 +101,15 @@ loader.load(
     }
 );
 
+// Raycaster 생성 : 객체와의 교차 감지
+const raycaster = new THREE.Raycaster();
+
+// 마우스 위치를 저장할 Vector2 객체 생성
+const mouse = new THREE.Vector2();
+
+
+let previousMaterial = null; // 이전에 선택된 객체의 재질을 저장할 변수
+let mouseInside = false; // 마우스가 화면 안에 있는지 여부를 나타내는 변수
 let colorTransitionFactor = 0; // 색상 전환 보간을 위한 변수
 let colorTransitionDirection = 1; // 색상 전환 방향: 1은 빨간색으로, -1은 원래 색상으로
 let targetColor = new THREE.Color(0xff0000); // 목표 색상 (빨간색)
